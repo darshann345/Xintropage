11ec255b70bb19738a3e2861d7d84126a4f15a6d
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-21 20:54:43 +0530

    committed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+
 function openMenu() {
-  document.getElementById("mobile-menu").style.display = "flex";
-}
+    document.getElementById("mobile-menu").style.display = "flex";
+    document.querySelector(".overlay").style.display = "block";
+  }
 
-function closeMenu() {
-  document.getElementById("mobile-menu").style.display = "none";
-}
-
-
+  function closeMenu() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.querySelector(".overlay").style.display = "none"; 
+  }
 
 
 
