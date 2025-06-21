de353204af02000b6cd5c31734fe426ef2206331
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-21 21:20:41 +0530

    committed

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-// ===== Mobile Menu Functions =====
+
 function openMenu() {
-  const menu = document.getElementById("mobile-menu");
-  const overlay = document.querySelector(".overlay");
-  menu.style.display = "flex";
-  overlay.style.display = "block";
-  overlay.style.opacity = "1";
-}
+    document.getElementById("mobile-menu").style.display = "flex";
+    document.querySelector(".overlay").style.display = "block";
+  }
 
-function closeMenu() {
-  const menu = document.getElementById("mobile-menu");
-  const overlay = document.querySelector(".overlay");
-  menu.style.display = "none";
-  overlay.style.opacity = "0";
-  setTimeout(() => {
-    overlay.style.display = "none";
-  }, 300);
-}
+  function closeMenu() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.querySelector(".overlay").style.display = "none"; 
+  }
 
-document.querySelector(".open-menu").addEventListener("click", openMenu);
-document.querySelector(".close-menu").addEventListener("click", closeMenu);
 
-const navLinks = document.querySelectorAll(".nav-link");
 
-navLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    const parent = link.closest(".dropdown-parent");
-    if (parent) {
-      e.preventDefault(); 
-      parent.classList.toggle("link-open");
-    }
-  });
-});
+
+  
