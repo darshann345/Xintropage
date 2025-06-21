f089b55d8fcb21123a70b062fa0a06d583b3d4e9
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-21 22:27:59 +0530

    committed

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".open-menu").addEventListener("click", openMenu);
+  document.querySelector(".close-menu").addEventListener("click", closeMenu);
+});
+
 function openMenu() {
   document.getElementById("mobile-menu").style.display = "flex";
-  document.querySelector(".overlay").style.display = "block";
 }
 
 function closeMenu() {
   document.getElementById("mobile-menu").style.display = "none";
-  document.querySelector(".overlay").style.display = "none";
-}
-
-const openBtn = document.getElementById("open-button");
-if (openBtn) {
-  openBtn.addEventListener("click", openMenu);
-}
-
-const closeBtn = document.getElementById("close-button");
-if (closeBtn) {
-  closeBtn.addEventListener("click", closeMenu);
 }
