e3c2a6d020274189c0c6f87dfa5e5b18232aa180
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-06-21 21:30:22 +0530

    committed

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-
 function openMenu() {
-    document.getElementById("mobile-menu").style.display = "flex";
-    document.querySelector(".overlay").style.display = "block";
-  }
+  document.getElementById("mobile-menu").style.display = "flex";
+  document.querySelector(".overlay").style.display = "block";
+}
 
-  function closeMenu() {
-    document.getElementById("mobile-menu").style.display = "none";
-    document.querySelector(".overlay").style.display = "none"; 
-  }
+function closeMenu() {
+  document.getElementById("mobile-menu").style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
+}
 
-
-
-
-  
+document.getElementById("open-button").addEventListener("click", openMenu);
+document.getElementById("close-button").addEventListener("click", closeMenu);
